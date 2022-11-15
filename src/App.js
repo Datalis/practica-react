b1735b8d1f08525de48a106d060929e84373c6b5
Rosa Amelia <lavieenrouse95@gmail.com>
2022-11-15 06:44:14 -0500

    Show user data

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  const user = {
+    name: "Rosa",
+    last_name: "Ocana"
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Nombre: {user.name}</p>
+        <p>Apellido: {user.last_name}</p>
       </header>
     </div>
   );
